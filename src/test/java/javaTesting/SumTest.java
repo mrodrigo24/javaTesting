d171931454edd863a13b7aaabaad139577b9df1e
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-04-26 20:29:25 +0200

    clase test

---
+++ src/test/java/javaTesting/SumTest.java
@@ -0,0 +1,15 @@
+package javaTesting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SumTest {
+    @Test
+    public void sumTest(){
+     int r=1+1;
+     assertEquals("1=1=2",2,r);
+
+
+    }
+}
