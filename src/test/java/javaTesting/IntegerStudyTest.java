76bd8ca852609518004af36e2b33305b39b19d83
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-05-01 22:58:37 +0200

    clase IntegerStudyTest

---
+++ src/test/java/javaTesting/IntegerStudyTest.java
@@ -0,0 +1,14 @@
+package javaTesting;
+import org.junit.Test;
+
+public class IntegerStudyTest {
+
+        @Test
+        public void IntegerStudy(){}
+        Integer vuit =new Integer(8);
+
+        Integer Tretze=13;
+        asserEquals("El numero tretze va relamente 13",13,tretze.intValue)
+    }
+
+
