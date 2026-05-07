640c48b6c298b7a0862164542bc4821d61e999c5
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-05-07 23:35:17 +0200

    seguimos con ArrayTesting

--- src/test/java/javaTesting/arrayTesting/ArrayTest.java
+++ src/test/java/javaTesting/arrayTesting/ArrayTest.java
@@ -16,6 +16,43 @@ public class ArrayTest {
        assertEquals(nombres,"zero|un|dos|tres|");
    }
 
+    @Test
+    public void farcirArrayParcialment(){
+       int[] nums ={0,1,2,3,4,5,6,7,8,9};
+       Arrays.fill(nums,5,10,-1);
+       for(int i=5;i<nums.length;i++){
+           assertTrue(nums[i]==-1);
+       }
+    }
+    @Test
+    public void copiaRangMatriu(){
+       String[] dies ={"dilluns","dimarts","dimecres","dijous","divendres","dissabte","diumenge"};
+       String[] feiners = Arrays.copyOfRange(dies,0,5);
+       assertTrue(feiners.length()==5);
+       assertTrue(feiners[1].equals("dimarts"));
+       String[] feinersEsclaus=Arrays.copyOfRange(feiners,0,7);
+       assertTrue(feinersEsclaus[0].equals("dilluns");
+       assertEquals(feinersEsclaus[5],null);
+       feinersEsclaus[5]="dissabteDeFeina";
+       feinersEsclaus[6]="diaDeDescans";
+       assertTrue(feinersEsclaus[6].equals("diaDeDescans"));
+   }
 
+   @Test
+    public void arraysIrregularsTest(){
+       int[][] arrayIrregular={
+               {0,1,2,3,4,5},
+               {0,1,2,3,4},
+               {0,1,2,3},
+               {0,1,2},
+               {0,1},
+               {0}
+
+
+       };
+       assertEquals(arrayIrregular.length,6);
+       assertEquals(arrayIrregular[0].length,6);
+       assertEquals(arrayIrregular[5].length,1);
+   }
 
 }
